ab6413f98965c99c92c96018529f6afb2b136ee6
pranav bhandari <pranav.dhiren@gmail>
2016-08-07 00:13:37 -0700

    added NSUserDefaults and delete functionality

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+var arr : [String] = []
+class FirstViewController: UIViewController, UITableViewDelegate {
 
-class FirstViewController: UIViewController {
-
+    @IBOutlet weak var tableView : UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if (NSUserDefaults.standardUserDefaults().objectForKey("todo") != nil) {
+            arr = NSUserDefaults.standardUserDefaults().objectForKey("todo") as! [String]
+        }
+        
     }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel!.text = arr[indexPath.row] 
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            arr.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "todo")
+            tableView.reloadData()
+        }
+    }
+    
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var newItem : UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.newItem.delegate = self
     }
 
+    @IBAction func addItem(sender: AnyObject) {
+        arr.append(newItem.text!)
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "todo")
+        newItem.text = ""
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(textField : UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
